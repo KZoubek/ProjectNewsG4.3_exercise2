4b5fe860b4315e6ef4cccf9235fca843e0244b86
Julian Wildauer <max.kell@tuta.io>
2022-04-14 15:44:23 +0200

    Article Variables Added with getters.

--- src/main/java/at/ac/fhcampuswien/models/Article.java
+++ src/main/java/at/ac/fhcampuswien/models/Article.java
@@ -1,12 +1,51 @@
 package at.ac.fhcampuswien.models;
 
+import java.util.Objects;
+
 public class Article {
     private String author;
     private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
 
-    public Article(String author, String title){
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    private String source;
+
+    public Article(String author, String title, String description, String url, String urlToImage, String publishedAt, String content, String source) {
         this.author = author;
         this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+        this.source = source;
     }
 
     public String getAuthor() {
@@ -33,7 +72,15 @@ public class Article {
 
     @Override
     public String toString() {
-        return "Title: " + getTitle() + "\n" +
-                "Author: " + getAuthor();
+        return "Article{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", url='" + url + '\'' +
+                ", urlToImage='" + urlToImage + '\'' +
+                ", publishedAt='" + publishedAt + '\'' +
+                ", content='" + content + '\'' +
+                ", source='" + source + '\'' +
+                '}';
     }
 }
