4283135089f98ab3d8076a39aa24b8b917b6953b
VjollcaB <71784141+VjollcaB@users.noreply.github.com>
2022-04-28 13:22:07 +0200

    comments and refactor in NewsResponse

--- src/main/java/at/ac/fhcampuswien/models/NewsResponse.java
+++ src/main/java/at/ac/fhcampuswien/models/NewsResponse.java
@@ -5,16 +5,19 @@ import at.ac.fhcampuswien.models.Article;
 import java.util.List;
 
 public class NewsResponse {
+    //private variables that are final because they are not changed and created only ones
     private final String status;
     private final int totalResults;
     private final List<Article> articles;
 
     public NewsResponse (String status,int totalResults, List<Article> articles){
+        //in the constructor, the variables are "assigned" to the parameters
         this.status = status;
         this.totalResults = totalResults;
         this.articles = articles;
     }
 
+    /*
     public String getStatus() {
         return status;
     }
@@ -22,13 +25,16 @@ public class NewsResponse {
     public int getTotalResults() {
         return totalResults;
     }
+        */
 
     public List<Article> getArticles() {
+        //with the getter, the articles are taken from the article class and returned
         return articles;
     }
 
     @Override
     public String toString() {
+        //the toString method returns the status, totalResults and article. (information from article = Article class, status & totalResults = NewsApi class)
         return "NewsResponse{" +
                 "status='" + status + '\'' +
                 ", totalResults=" + totalResults +
